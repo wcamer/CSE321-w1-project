92bf4c3d6bfa9253c982e0ca417736249e8f16ee
wcamer <xeus.xiii@hotmail.com>
2024-04-25 21:23:49 -0600

    updates for the getting a single contact

--- controllers/contactsCon.js
+++ controllers/contactsCon.js
@@ -13,9 +13,23 @@ const getAll = async (req,res) =>{
     const contacts = await collection.toArray()
     res.setHeader('Content-Type', 'application/json');
     res.json(contacts)
-    console.log("here is collection....\n",contacts)
+    // console.log("here is collection....\n",contacts)
 }
 
-module.exports = {getAll}
+
+const mongodbObjectId = require('mongodb').ObjectId
+const getOne = async (req,res) =>{
+    const id = new mongodbObjectId(req.params.id)
+    // console.log("here is req.params",req.params)
+    // console.log("here is the req.parms.id",req.params.id)
+    const collection = await mongoDb.getDb().db().collection('contacts').find({_id: id})
+    const singleContact = await collection.toArray()
+
+    res.setHeader('Content-Type', 'application/json')
+    res.json(singleContact[0])
+    // console.log("here is single contact....\n",singleContact)
+}
+
+module.exports = {getAll, getOne}
 
 
--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const contactsController = require('../controllers/contactsCon')
 
 router.get('/' ,contactsController.getAll)
+router.get('/:id' ,contactsController.getOne)
 
 module.exports = router
